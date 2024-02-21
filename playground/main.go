fa680443385ac72774b9137470727f3018633956
Laika <1317280699@qq.com>
2024-02-21 21:11:19 +0800

    playground

--- playground/main.go
+++ playground/main.go
@@ -2,35 +2,7 @@ package main
 
 import "fmt"
 
-type CustomStruct struct {
-	A int
-}
-
-type Options func(*CustomStruct)
-
-func WithAddValue(value int) Options {
-	return func(c *CustomStruct) {
-		c.A += value
-	}
-}
-func WithMinusValue(value int) Options {
-	return func(c *CustomStruct) {
-		c.A -= value
-	}
-}
-func NewCustomStruct(initValue int, opts ...Options) *CustomStruct {
-	res := &CustomStruct{
-		A: initValue,
-	}
-
-	for _, opt := range opts {
-		opt(res)
-	}
-	return res
-}
-
 func main() {
-
-	stru := NewCustomStruct(5, WithAddValue(-1), WithAddValue(2), WithMinusValue(2))
-	fmt.Println(stru.A)
+	s := []int{1, 2, 3, 4, 5}
+	fmt.Println(s[2:5])
 }
